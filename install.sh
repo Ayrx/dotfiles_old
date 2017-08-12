5e31890e1fa583d6a14421a9f020e9d2ad3e6aff
Terry Chia <terrycwk1994@gmail.com>
2017-08-12 22:01:44 +0800

    Add .tmux.conf.

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ CWD=$(pwd)
 # Run vim related configs
 ln -sf $CWD/vim/.vimrc $HOME/.vimrc
 ./vim/install_vim_plugins.sh
+
+# Move tmux config file into place
+ln -sf $CWD/.tmux.conf $HOME/.tmux.conf
