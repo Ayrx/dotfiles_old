4d7279c5822822b8159bdb129de0112810d67658
Terry Chia <terrycwk1994@gmail.com>
2018-08-28 12:44:14 +0800

    Add git configurations.

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ ln -sf $CWD/vim/init.vim $HOME/.config/nvim/init.vim
 
 # Move tmux config file into place
 ln -sf $CWD/tmux/.tmux.conf $HOME/.tmux.conf
+
+# Move .gitconfig config file into place
+ln -sf $CWD/git/.gitconfig $HOME/.gitconfig
+ln -sf $CWD/git/.gitignore_global $HOME/.gitignore_global
