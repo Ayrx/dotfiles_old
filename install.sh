d6636c7b4fdb4e90e3b856e566dacc7cf01c930d
Terry Chia <terrycwk1994@gmail.com>
2020-03-07 17:27:10 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CWD=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/fish
-ln -sf $CWD/vim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $CWD/tmux/.tmux.conf $HOME/.tmux.conf
-ln -sf $CWD/git/.gitconfig $HOME/.gitconfig
-ln -sf $CWD/git/.gitignore_global $HOME/.gitignore_global
-ln -sf $CWD/fish/config.fish $HOME/.config/fish/config.fish
+ln -sf $DIR/vim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $DIR/tmux/.tmux.conf $HOME/.tmux.conf
+ln -sf $DIR/git/.gitconfig $HOME/.gitconfig
+ln -sf $DIR/git/.gitignore_global $HOME/.gitignore_global
+ln -sf $DIR/fish/config.fish $HOME/.config/fish/config.fish
