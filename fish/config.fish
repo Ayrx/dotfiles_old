88d1325de4108bb06b0a0626966d3b2fd2693461
Terry Chia <terrycwk1994@gmail.com>
2020-06-16 11:27:44 +0800

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,7 @@ set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 # Others
 set -x PYTHONDONTWRITEBYTECODE 1
 set PATH ~/tools/bin $PATH
+set PATH ~/tools/ansible_bin $PATH
 
 # Functions
 function ara
